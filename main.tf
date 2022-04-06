812eb01d703096a3515059a83743cbcc1843083e
Majid S <m.sotoodeh@gmail.com>
2022-04-05 22:32:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "iks" {
 # Kubernetes Cluster Profile  Adjust the values as needed.
   cluster = {
     name                = "IKS-Cluster01"
-    action              = "Unassign"
+    action              = "Deploy"
     wait_for_completion = false
     worker_nodes        = 2
     load_balancers      = 1
